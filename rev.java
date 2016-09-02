12db8a24cb327c6b777a68f1ebf32dc56f88bf62
Selvaranikuppuswami <ksraniswami@gmail.com>
2016-09-02 12:12:05 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class rev
+{
+public static void main(String[] args)
+{
+Scanner sb=new Scanner(System.in);
+string s=sb.next();
+StringBuffer sb=new StringBuffer(s);
+sb.reverse();
+System.out.println(sb);
+}
+}
