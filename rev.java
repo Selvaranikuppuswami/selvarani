eb52fc85f4e8bf403aa63f508960a7a8125d4237
Selvaranikuppuswami <ksraniswami@gmail.com>
2016-09-02 12:23:33 +0530

    Update rev.java

--- rev.java
+++ rev.java
@@ -4,9 +4,9 @@ public class rev
 public static void main(String[] args)
 {
 Scanner sb=new Scanner(System.in);
-string s=sb.next();
-StringBuffer sb=new StringBuffer(s);
-sb.reverse();
-System.out.println(sb);
+String s=sb.next();
+StringBuffer sc=new StringBuffer(s);
+sc.reverse();
+System.out.println(sc);
 }
 }
